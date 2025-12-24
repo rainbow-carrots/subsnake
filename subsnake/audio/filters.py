6d91cee9ae7d6cca317f13878dc86e5ef42d274e
rae <ramonaauble@gmail.com>
2025-12-24 02:53:47 -0500

    mono -> stereo

--- subsnake/audio/filters.py
+++ subsnake/audio/filters.py
@@ -20,44 +20,47 @@ class HalSVF():
         f = 2*math.sin(np.pi*(cutoff/(4*fs)))
 
         #init parameter buffer | lowpass, bandpass, tuning, dampening, type, drive
-        self.state = np.array([0.0, 0.0, f, q, type, drive], dtype=np.float32)
+        self.state = np.array([[0.0, 0.0, f, q, type, drive], [0.0, 0.0, f, q, type, drive]], dtype=np.float32)
     
     def process_block(self, state, input, output):
         self.filter_block(state, input, output)
 
     def update_cutoff(self, freq):
         f = 2*math.sin(np.pi*(freq/(4*fs)))
-        self.state[2] = f
+        self.state[0, 2] = f
+        self.state[1, 2] = f
 
     def update_resonance(self, res):
-        self.state[3] = res
+        self.state[0, 3] = res
+        self.state[1, 3] = res
 
     def update_drive(self, drive):
-        self.state[5] = drive
+        self.state[0, 5] = drive
     
     def update_type(self, type):
-        self.state[4] = type
+        self.state[0, 4] = type
+        self.state[1, 4] = type
         
     @staticmethod
     @njit(nogil=True, fastmath=True)
     def filter_block(state, input, output):
-        for n in range(len(output)):
-            subsample = 0.0
-            substate = state[4]
-            for m in range(4):
-                state[0] = state[0] + state[2]*state[1]
-                feedback = state[3]*(np.tanh(state[1]*state[5])/state[5])
-                high = input[n, 0] - state[0] - feedback
-                state[1] = state[2]*high + state[1]
-                notch = high + state[0]
-                if (substate == 0.0):   #lowpass
-                    subsample += state[0]
-                elif (substate == 1.0): #highpass
-                    subsample += high
-                elif (substate == 2.0): #bandpass
-                    subsample += state[1]
-                elif (substate == 3.0): #notch
-                    subsample += notch
-            sample = subsample*0.25
-            output[n, 0] = sample
-            output[n, 1] = sample
+        for c in range (2):
+            for n in range(len(output)):
+                subsample = 0.0
+                substate = state[c, 4]
+                for m in range(4):
+                    state[c, 0] = state[c, 0] + state[c, 2]*state[c, 1]
+                    feedback = state[c, 3]*(np.tanh(state[c, 1]*state[c, 5])/state[c, 5])
+                    high = input[n, 0] - state[c, 0] - feedback
+                    state[c, 1] = state[c, 2]*high + state[c, 1]
+                    notch = high + state[c, 0]
+                    if (substate == 0.0):   #lowpass
+                        subsample += state[c, 0]
+                    elif (substate == 1.0): #highpass
+                        subsample += high
+                    elif (substate == 2.0): #bandpass
+                        subsample += state[c, 1]
+                    elif (substate == 3.0): #notch
+                        subsample += notch
+                sample = subsample*0.25
+                output[n, c] = sample
