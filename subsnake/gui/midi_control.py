a5aa485df92a7702cc9e36ef84bbae98921fe241
rae <ramonaauble@gmail.com>
2026-01-11 23:53:23 -0500

    + cc_deleted signal, + row attribute, + delete_cc slot, set object name, remove column QLabels

--- subsnake/gui/midi_control.py
+++ subsnake/gui/midi_control.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import (
-    QLabel, QSpinBox, QComboBox,
+    QPushButton, QSpinBox, QComboBox,
     QGridLayout, QWidget
 )
 from PySide6.QtCore import Signal
@@ -9,22 +9,22 @@ class MIDIControl(QWidget):
     #signals
     param_changed = Signal(int, str)
     cc_changed = Signal(int, int, str)
+    cc_deleted = Signal(int, int)
     
     def __init__(self):
         super().__init__()
 
         #attributes
         self.prev_cc = 0
+        self.row = 0
 
         #layout
         layout = QGridLayout()
         
         #widgets
-        cc_label = QLabel("CC:")
+        self.cc_delete = QPushButton("-")
         self.cc_select = QSpinBox()
-        module_label = QLabel("module:")
         self.module_select = QComboBox()
-        param_label = QLabel("parameter:")
         self.param_select = QComboBox()
 
         #param name LUT
@@ -39,19 +39,19 @@ class MIDIControl(QWidget):
         self.module_select.setCurrentIndex(0)
         self.param_select.addItems(self.param_names[0])
         self.param_select.setCurrentIndex(0)
+        self.cc_delete.setObjectName("cc_delete")
 
         #add widgets to layout
-        layout.addWidget(cc_label, 0, 0)
+        layout.addWidget(self.cc_delete, 0, 0)
         layout.addWidget(self.cc_select, 0, 1)
-        layout.addWidget(module_label, 0, 2)
-        layout.addWidget(self.module_select, 0, 3)
-        layout.addWidget(param_label, 0, 4)
-        layout.addWidget(self.param_select, 0, 5)
+        layout.addWidget(self.module_select, 0, 2)
+        layout.addWidget(self.param_select, 0, 3)
         
         #connect signals
         self.module_select.currentIndexChanged.connect(self.update_params_list)
         self.param_select.currentTextChanged.connect(self.update_param)
         self.cc_select.valueChanged.connect(self.update_cc)
+        self.cc_delete.pressed.connect(self.delete_cc)
 
         #set widget layout
         self.setLayout(layout)
@@ -67,3 +67,6 @@ class MIDIControl(QWidget):
     def update_cc(self, new_cc):
         self.cc_changed.emit(new_cc, self.prev_cc, self.param_select.currentText())
         self.prev_cc = new_cc
+
+    def delete_cc(self):
+        self.cc_deleted.emit(self.cc_select.value(), self.row)
