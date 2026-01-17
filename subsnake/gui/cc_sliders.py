ceaea30253ec6778bcefa2b0d3bfb2db7715edeb
rae <ramonaauble@gmail.com>
2026-01-16 21:48:02 -0500

    + logic to update param displays, add assign_cc_display helper, add update_cc_display to update displays from scaled & formatted cc values

--- subsnake/gui/cc_sliders.py
+++ subsnake/gui/cc_sliders.py
@@ -8,6 +8,7 @@ class UpdateSliders(QTimer):
         self.row_ccs = window.midi_group.row_ccs
         self.cc_values = engine.midi_cc_values
         self.cc_sliders = window.midi_cc_sliders
+        self.cc_displays = window.midi_cc_displays
         self.setInterval(17)   #~60fps
         self.timeout.connect(self.update_sliders)
 
@@ -21,6 +22,10 @@ class UpdateSliders(QTimer):
                     self.cc_sliders[cc].blockSignals(True)
                     self.cc_sliders[cc].setValue(scaled_value)
                     self.cc_sliders[cc].blockSignals(False)
+                    if cc in self.cc_displays:
+                        display, module, param = self.cc_displays[cc]
+                        self.update_cc_display(display, module, param, scaled_value)
+
 
     def assign_cc_slider(self, module, param):
         cc_slider = None
@@ -70,3 +75,94 @@ class UpdateSliders(QTimer):
             elif (param == "release"):
                 cc_slider = self.window.env_group.adsr_rel_slider
         return cc_slider
+    
+    def assign_cc_display(self, module, param):
+        cc_display = None
+        if (module == "oscillator 1"):
+            if (param == "pitch"):
+                cc_display = self.window.osc_group.osc_freq_display
+            elif (param == "level"):
+                cc_display = self.window.osc_group.osc_amp_display
+            elif (param == "width"):
+                cc_display = self.window.osc_group.osc_width_display
+        elif (module == "oscillator 2"):
+            if (param == "pitch"):
+                cc_display = self.window.osc2_group.osc2_freq_display
+            elif (param == "detune"):
+                cc_display = self.window.osc2_group.osc2_det_display
+            elif (param == "level"):
+                cc_display = self.window.osc2_group.osc2_amp_display
+            elif (param == "width"):
+                cc_display = self.window.osc2_group.osc2_width_display
+        elif (module == "filter"):
+            if (param == "cutoff"):
+                cc_display = self.window.filt_group.filt_freq_display
+            elif (param == "feedback"):
+                cc_display = self.window.filt_group.filt_fback_display
+            elif (param == "drive"):
+                cc_display = self.window.filt_group.filt_drive_display
+            elif (param == "saturate"):
+                cc_display = self.window.filt_group.filt_sat_display
+        elif (module == "filter env"):
+            if (param == "attack"):
+                cc_display = self.window.fenv_group.fenv_att_display
+            elif (param == "decay"):
+                cc_display = self.window.fenv_group.fenv_dec_display
+            elif (param == "sustain"):
+                cc_display = self.window.fenv_group.fenv_sus_display
+            elif (param == "release"):
+                cc_display = self.window.fenv_group.fenv_rel_display
+            elif (param == "depth"):
+                cc_display = self.window.fenv_group.fenv_amt_display
+        elif (module == "envelope"):
+            if (param == "attack"):
+                cc_display = self.window.env_group.adsr_att_display
+            elif (param == "decay"):
+                cc_display = self.window.env_group.adsr_dec_display
+            elif (param == "sustain"):
+                cc_display = self.window.env_group.adsr_sus_display
+            elif (param == "release"):
+                cc_display = self.window.env_group.adsr_rel_display
+        return cc_display
+    
+    def update_cc_display(self, display, module, param, value):
+        if (module == "oscillator 1"):
+            if (param == "pitch"):
+                display_val = float(value)/100.0
+            elif (param == "level"):
+                display_val = float(value)/500.0
+            elif (param == "width"):
+                display_val = float(value)/500.0
+            display.display(f"{display_val:.2f}")
+        elif (module == "oscillator 2"):
+            if (param == "pitch"):
+                display_val = float(value)/100.0
+            elif (param == "detune"):
+                display_val = value/20.0
+            elif (param == "level"):
+                display_val = float(value)/500.0
+            elif (param == "width"):
+                display_val = float(value)/500.0
+            display.display(f"{display_val:.2f}")
+        elif (module == "filter"):
+            if (param == "cutoff"):
+                display_val = 27.5 * 2**(float(value)/100.0)
+                display.display(f"{display_val:.1f}")
+            elif (param == "feedback"):
+                display_val = 10.0 / (10.0**(value/100.0))
+                display.display(f"{1.0/display_val:.2f}")
+            elif (param == "drive"):
+                display_val = value/40.0
+                display.display(f"{display_val:.2f}")
+            elif (param == "saturate"):
+                display_val = float(value)/100.0
+                display.display(f"{display_val:.2f}")
+        elif (module == "filter env"):
+            if (param == "depth"):
+                display_val = float(value)/100.0
+            else:
+                display_val = float(value)/1000.0
+            display.display(f"{display_val:.2f}")
+        elif (module == "envelope"):
+            display_val = float(value)/1000.0
+            display.display(f"{display_val:.2f}")
