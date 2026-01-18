94b9681f8a0aff18d471a77881ed1d657bb5c9bc
rae <ramonaauble@gmail.com>
2026-01-18 00:24:56 -0500

    add **StereoDelay** class - simple stereo single-tap delay with feedback

---
+++ subsnake/audio/effects.py
@@ -0,0 +1,32 @@
+import numpy as np
+from numba import njit
+
+class StereoDelay():
+    def __init__(self, fs, delay_time=0.67, delay_feedback=0.5, mix=.5):
+        samples = fs*5      #5s delay
+        self.fs = fs
+        self.buffer = np.zeros((samples, 2), dtype=np.float32)
+        self.delay_time = delay_time
+        self.delay_feedback = delay_feedback
+        self.write_heads = [0, 0]
+        self.offset = 22050
+        self.mix_level = mix
+
+    def process_block(self, input, output):
+        self.offset = int(self.delay_time*self.fs)
+        self.delay_block(input, output, self.buffer, self.offset, self.write_heads, self.delay_feedback, self.mix_level)
+
+    @staticmethod
+    @njit(nogil=True, fastmath=True)
+    def delay_block(input, output, buffer, offset, write_heads, feedback, mix):
+        frames = len(output)
+        for c in range (0, 2):
+            for n in range(0, frames):
+                delayed_sample = buffer[write_heads[c], c]
+                buffer[write_heads[c], c] = delayed_sample*feedback + input[n, c]
+                write_heads[c] += 1
+                if (write_heads[c] >= offset):
+                    write_heads[c] -= offset
+                output[n, c] = (1.0-mix)*input[n, c] + mix*buffer[write_heads[c], c]
+            
+
