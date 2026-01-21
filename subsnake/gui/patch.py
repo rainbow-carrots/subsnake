f60dc0b1daf1a929360df4d95a5a603e29ac13eb
rae <ramonaauble@gmail.com>
2026-01-21 15:52:00 -0500

    import **resources**, **json** and patch directory, init *patch_select* with list of patch names, add *load_patch* slot to load patch data from selected file, add *patch_loaded* signal to send loaded patch data to window

--- subsnake/gui/patch.py
+++ subsnake/gui/patch.py
@@ -3,12 +3,17 @@ from PySide6.QtWidgets import (
     QWidget, QComboBox, QPushButton,
     QHBoxLayout
 )
+from importlib import resources
+import json
+import subsnake.patches
 
 class PatchManager(QWidget):
+    patch_loaded = Signal(dict)
     def __init__(self):
         super().__init__()
 
         self.patch_select = QComboBox()
+        self.patch_select.setFocusPolicy(Qt.NoFocus)
         self.save_patch = QPushButton("save")
         self.new_patch = QPushButton("new")
 
@@ -25,3 +30,24 @@ class PatchManager(QWidget):
 
         self.setObjectName("patch_manager")
         self.setAttribute(Qt.WA_StyledBackground, True)
+
+        #get path to patches
+        self.patch_path = resources.files(subsnake.patches)
+        self.patch_names = []
+
+        #init patch_select
+        for patch in self.patch_path.iterdir():
+            if patch.is_file() and patch.name.endswith(".json"):
+                filename = patch.name.removesuffix(".json")
+                self.patch_names.append(filename)
+        self.patch_select.addItems(self.patch_names)
+
+        self.patch_select.currentTextChanged.connect(self.load_patch)
+
+    def load_patch(self, patch_name):
+        patch_file = patch_name + ".json"
+        patch_path = self.patch_path / patch_file
+        with open(patch_path, "r") as f:
+            patch_data = json.load(f)
+        self.patch_loaded.emit(patch_data)
+
