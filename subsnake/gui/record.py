6b33c45109b5b9d2b3c58b36c4e30c42c4c4cda3
rae <ramonaauble@gmail.com>
2026-01-22 20:06:48 -0500

    add **RecorderGUI** class - for recording/looping

---
+++ subsnake/gui/record.py
@@ -0,0 +1,60 @@
+from PySide6.QtWidgets import (
+    QWidget, QPushButton, QHBoxLayout,
+    QLabel, QSizePolicy, QFrame
+)
+from PySide6.QtCore import Qt, Signal
+
+class RecorderGUI(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.current_time_label = QLabel("00:00")
+        slash_label = QLabel("/")
+        self.end_time_label = QLabel(("01:23"))
+
+        self.record_button = QPushButton("●︎")
+        self.play_button = QPushButton("▶")
+        self.stop_button = QPushButton("■")
+        self.loop_button = QPushButton("⟳")
+
+        self.record_button.setObjectName("record_button")
+        self.play_button.setObjectName("play_button")
+        self.stop_button.setObjectName("stop_button")
+        self.loop_button.setObjectName("loop_button")
+
+        self.current_time_label.setAlignment(Qt.AlignCenter)
+        self.end_time_label.setAlignment(Qt.AlignCenter)
+        slash_label.setAlignment(Qt.AlignCenter)
+
+        self.current_time_label.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        self.end_time_label.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        slash_label.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+
+        spacer_0 = QFrame()
+        spacer_0.setFrameShape(QFrame.NoFrame)
+        spacer_0.setObjectName("record_spacer_0")
+        spacer_1 = QFrame()
+        spacer_1.setFrameShape(QFrame.NoFrame)
+        spacer_1.setObjectName("record_spacer_1")
+        spacer_2 = QFrame()
+        spacer_2.setFrameShape(QFrame.NoFrame)
+        spacer_2.setObjectName("record_spacer_2")
+
+
+        layout = QHBoxLayout()
+        layout.addStretch()
+        layout.addWidget(self.record_button, 0)
+        layout.addWidget(spacer_0)
+        layout.addWidget(self.current_time_label, 1)
+        layout.addWidget(slash_label, 2)
+        layout.addWidget(self.end_time_label, 3)
+        layout.addWidget(spacer_1)
+        layout.addWidget(self.play_button, 4)
+        layout.addWidget(self.stop_button, 5)
+        layout.addWidget(spacer_2)
+        layout.addWidget(self.loop_button, 6)
+        layout.addStretch()
+
+        self.setLayout(layout)
+        self.setObjectName("recorder")
+        self.setAttribute(Qt.WA_StyledBackground, True)
