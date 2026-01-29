afb7fceed65fdad7f97a6f01bcea0de39e0dd919
rae <ramonaauble@gmail.com>
2026-01-29 18:42:02 -0500

    **RecorderGUI**: add *delete* signal, add *delete_button* & set object name, add *spacer_3*, add delete button & spacer to layout, connect *delete_button* signal, add delete_pressed() slot, add logic to enable/disable pushbuttons

--- subsnake/gui/record.py
+++ subsnake/gui/record.py
@@ -5,6 +5,7 @@ from PySide6.QtWidgets import (
 from PySide6.QtCore import Qt, Signal
 
 class RecorderGUI(QWidget):
+    delete = Signal()
     pause = Signal()
     play = Signal()
     stop = Signal()
@@ -18,6 +19,7 @@ class RecorderGUI(QWidget):
         slash_label = QLabel("/")
         self.end_time_label = QLabel(("01:23"))
 
+        self.delete_button = QPushButton("␡")
         self.record_button = QPushButton("●︎")
         self.play_button = QPushButton("▶")
         self.stop_button = QPushButton("■")
@@ -27,6 +29,7 @@ class RecorderGUI(QWidget):
         self.loop_button.setCheckable(True)
         self.play_button.setCheckable(True)
 
+        self.delete_button.setObjectName("delete_button")
         self.record_button.setObjectName("record_button")
         self.play_button.setObjectName("play_button")
         self.stop_button.setObjectName("stop_button")
@@ -42,44 +45,59 @@ class RecorderGUI(QWidget):
 
         spacer_0 = QFrame()
         spacer_0.setFrameShape(QFrame.NoFrame)
-        spacer_0.setObjectName("record_spacer_0")
+        spacer_0.setObjectName("record_spacer")
         spacer_1 = QFrame()
         spacer_1.setFrameShape(QFrame.NoFrame)
-        spacer_1.setObjectName("record_spacer_1")
+        spacer_1.setObjectName("record_spacer")
         spacer_2 = QFrame()
         spacer_2.setFrameShape(QFrame.NoFrame)
-        spacer_2.setObjectName("record_spacer_2")
+        spacer_2.setObjectName("record_spacer")
+        spacer_3 = QFrame()
+        spacer_3.setFrameShape(QFrame.NoFrame)
+        spacer_3.setObjectName("record_spacer")
 
 
         layout = QHBoxLayout()
         layout.addStretch()
-        layout.addWidget(self.record_button, 0)
+        layout.addWidget(self.delete_button, 0)
+        layout.addWidget(self.record_button, 1)
         layout.addWidget(spacer_0)
-        layout.addWidget(self.current_time_label, 1)
-        layout.addWidget(slash_label, 2)
-        layout.addWidget(self.end_time_label, 3)
+        layout.addWidget(self.current_time_label, 2)
+        layout.addWidget(slash_label, 3)
+        layout.addWidget(self.end_time_label, 4)
         layout.addWidget(spacer_1)
-        layout.addWidget(self.play_button, 4)
-        layout.addWidget(self.stop_button, 5)
+        layout.addWidget(self.play_button, 5)
+        layout.addWidget(self.stop_button, 6)
         layout.addWidget(spacer_2)
-        layout.addWidget(self.loop_button, 6)
+        layout.addWidget(self.loop_button, 7)
         layout.addStretch()
 
         self.setLayout(layout)
         self.setObjectName("recorder")
         self.setAttribute(Qt.WA_StyledBackground, True)
 
+        self.delete_button.pressed.connect(self.delete_pressed)
         self.record_button.clicked.connect(self.record_pressed)
         self.play_button.clicked.connect(self.play_pressed)
         self.stop_button.pressed.connect(self.stop_pressed)
         self.loop_button.clicked.connect(self.loop_pressed)
 
+    def delete_pressed(self):
+        self.play_button.setDisabled(True)
+        self.loop_button.setDisabled(True)
+        self.delete.emit()
+
     def record_pressed(self, state):
+        if not self.play_button.isEnabled():
+            self.play_button.setDisabled(False)
+        if not self.loop_button.isEnabled():
+            self.loop_button.setDisabled(False)
         self.record.emit(state)
 
     def play_pressed(self, state):
         if state:
             self.play.emit()
+            self.delete_button.setDisabled(True)
         else:
             self.pause.emit()
 
@@ -88,7 +106,14 @@ class RecorderGUI(QWidget):
         self.play_button.setChecked(False)
         self.record_pressed(False)
         self.record_button.setChecked(False)
+        if not self.loop_button.isChecked():
+            self.delete_button.setDisabled(False)
         self.stop.emit()
 
     def loop_pressed(self, state):
+        if state:
+            self.delete_button.setDisabled(True)
+        else:
+            if not self.play_button.isChecked():
+                self.delete_button.setDisabled(False)
         self.loop.emit(state)
