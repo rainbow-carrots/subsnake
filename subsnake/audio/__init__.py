5e9475d5711f17de137cc6028df87472c55acd58
rae <ramonaauble@gmail.com>
2026-01-31 22:39:52 -0500

    expose **LFO** class

--- subsnake/audio/__init__.py
+++ subsnake/audio/__init__.py
@@ -3,4 +3,6 @@ from .generators import WrappedOsc
 from .envelopes import ADSR
 from .engine import AudioEngine
 from .workers import KeyEventWorker
-from .effects import StereoDelay
+from .effects import StereoDelay, AudioRecorder
+from .modulators import LFO
+
