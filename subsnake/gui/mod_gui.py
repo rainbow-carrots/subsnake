04695c30a7eb46dd0612c41daf3b88348cb82d82
rae <ramonaauble@gmail.com>
2026-02-03 23:05:17 -0500

    **ModulatorGUI**: add signals, add slots, connect internal signals to slots, change *lfo_phase_slider_x* min value from 1 -> 0, add ramp & s&h buttons to lfos 1 & 2, change shape select button titles

--- subsnake/gui/mod_gui.py
+++ subsnake/gui/mod_gui.py
@@ -10,6 +10,23 @@ from PySide6.QtWidgets import (
 from subsnake.gui.lcd import ClickLCD
 
 class ModulatorGUI(QGroupBox):
+    #signals
+    lfo1_freq_changed = Signal(float)
+    lfo1_offset_changed = Signal(float)
+    lfo1_shape_changed = Signal(int)
+
+    lfo2_freq_changed = Signal(float)
+    lfo2_offset_changed = Signal(float)
+    lfo2_shape_changed = Signal(int)
+
+    menv1_att_changed = Signal(float)
+    menv1_rel_changed = Signal(float)
+    menv1_mode_changed = Signal(int)
+
+    menv2_att_changed = Signal(float)
+    menv2_rel_changed = Signal(float)
+    menv2_mode_changed = Signal(int)   
+
     def __init__(self, display_color=QColor("black")):
         super().__init__()
         self.display_color = display_color
@@ -40,7 +57,7 @@ class ModulatorGUI(QGroupBox):
         self.lfo_freq_slider_1 = QSlider(Qt.Horizontal)
         self.lfo_phase_slider_1 = QSlider(Qt.Horizontal)
         self.lfo_freq_slider_1.setRange(1, 1000)
-        self.lfo_phase_slider_1.setRange(1, 800)
+        self.lfo_phase_slider_1.setRange(0, 1000)
         self.lfo_freq_slider_1.setSingleStep(1)
         self.lfo_freq_slider_1.setSingleStep(1)
 
@@ -49,22 +66,30 @@ class ModulatorGUI(QGroupBox):
         self.lfo_shape_layout_1 = QHBoxLayout()
         self.lfo_sin_button_1 = QRadioButton("sine")
         self.lfo_sin_button_1.setChecked(True)
-        self.lfo_tri_button_1 = QRadioButton("triangle")
-        self.lfo_saw_button_1 = QRadioButton("sawtooth")
+        self.lfo_tri_button_1 = QRadioButton("tri")
+        self.lfo_rmp_button_1 = QRadioButton("ramp")
+        self.lfo_saw_button_1 = QRadioButton("saw")
         self.lfo_sqr_button_1 = QRadioButton("square")
+        self.lfo_sah_button_1 = QRadioButton("s+h")
         self.lfo_shape_buttons_1.addButton(self.lfo_sin_button_1)
         self.lfo_shape_buttons_1.addButton(self.lfo_tri_button_1)
+        self.lfo_shape_buttons_1.addButton(self.lfo_rmp_button_1)
         self.lfo_shape_buttons_1.addButton(self.lfo_saw_button_1)
         self.lfo_shape_buttons_1.addButton(self.lfo_sqr_button_1)
+        self.lfo_shape_buttons_1.addButton(self.lfo_sah_button_1)
 
         self.lfo_shape_layout_1.addStretch()
         self.lfo_shape_layout_1.addWidget(self.lfo_sin_button_1)
         self.lfo_shape_layout_1.addStretch()
         self.lfo_shape_layout_1.addWidget(self.lfo_tri_button_1)
         self.lfo_shape_layout_1.addStretch()
+        self.lfo_shape_layout_1.addWidget(self.lfo_rmp_button_1)
+        self.lfo_shape_layout_1.addStretch()
         self.lfo_shape_layout_1.addWidget(self.lfo_saw_button_1)
         self.lfo_shape_layout_1.addStretch()
         self.lfo_shape_layout_1.addWidget(self.lfo_sqr_button_1)
+        self.lfo_shape_layout_1.addStretch()
+        self.lfo_shape_layout_1.addWidget(self.lfo_sah_button_1)
         self.lfo_shape_layout_1.addStretch()
 
         #   displays
@@ -97,7 +122,7 @@ class ModulatorGUI(QGroupBox):
         self.lfo_freq_slider_2 = QSlider(Qt.Horizontal)
         self.lfo_phase_slider_2 = QSlider(Qt.Horizontal)
         self.lfo_freq_slider_2.setRange(1, 1000)
-        self.lfo_phase_slider_2.setRange(1, 800)
+        self.lfo_phase_slider_2.setRange(0, 1000)
         self.lfo_freq_slider_2.setSingleStep(1)
         self.lfo_freq_slider_2.setSingleStep(1)
 
@@ -106,22 +131,30 @@ class ModulatorGUI(QGroupBox):
         self.lfo_shape_layout_2 = QHBoxLayout()
         self.lfo_sin_button_2 = QRadioButton("sine")
         self.lfo_sin_button_2.setChecked(True)
-        self.lfo_tri_button_2 = QRadioButton("triangle")
-        self.lfo_saw_button_2 = QRadioButton("sawtooth")
+        self.lfo_tri_button_2 = QRadioButton("tri")
+        self.lfo_rmp_button_2 = QRadioButton("ramp")
+        self.lfo_saw_button_2 = QRadioButton("saw")
         self.lfo_sqr_button_2 = QRadioButton("square")
+        self.lfo_sah_button_2 = QRadioButton("s+h")
         self.lfo_shape_buttons_2.addButton(self.lfo_sin_button_2)
+        self.lfo_shape_buttons_2.addButton(self.lfo_rmp_button_2)
         self.lfo_shape_buttons_2.addButton(self.lfo_tri_button_2)
         self.lfo_shape_buttons_2.addButton(self.lfo_saw_button_2)
         self.lfo_shape_buttons_2.addButton(self.lfo_sqr_button_2)
+        self.lfo_shape_buttons_2.addButton(self.lfo_sah_button_2)
 
         self.lfo_shape_layout_2.addStretch()
         self.lfo_shape_layout_2.addWidget(self.lfo_sin_button_2)
         self.lfo_shape_layout_2.addStretch()
         self.lfo_shape_layout_2.addWidget(self.lfo_tri_button_2)
         self.lfo_shape_layout_2.addStretch()
+        self.lfo_shape_layout_2.addWidget(self.lfo_rmp_button_2)
+        self.lfo_shape_layout_2.addStretch()
         self.lfo_shape_layout_2.addWidget(self.lfo_saw_button_2)
         self.lfo_shape_layout_2.addStretch()
         self.lfo_shape_layout_2.addWidget(self.lfo_sqr_button_2)
+        self.lfo_shape_layout_2.addStretch()
+        self.lfo_shape_layout_2.addWidget(self.lfo_sah_button_2)
         self.lfo_shape_layout_2.addStretch()
 
         #   displays
@@ -309,7 +342,33 @@ class ModulatorGUI(QGroupBox):
         self.menv_button_2.setObjectName("menv2_button")
         self.setTitle("modulators")
 
+        #connect signals
         self.mod_buttons_group.buttonClicked.connect(self.setModule)
+        # lfo 1
+        self.lfo_freq_slider_1.valueChanged.connect(self.change_lfo1_freq)
+        self.lfo_phase_slider_1.valueChanged.connect(self.change_lfo1_offset)
+        self.lfo_shape_buttons_1.buttonClicked.connect(self.change_lfo1_shape)
+        self.lfo_freq_display_1.double_clicked.connect(self.reset_lfo1_freq)
+        self.lfo_phase_display_1.double_clicked.connect(self.reset_lfo1_offset)
+        # lfo 2
+        self.lfo_freq_slider_2.valueChanged.connect(self.change_lfo2_freq)
+        self.lfo_phase_slider_2.valueChanged.connect(self.change_lfo2_offset)
+        self.lfo_shape_buttons_2.buttonClicked.connect(self.change_lfo2_shape)
+        self.lfo_freq_display_2.double_clicked.connect(self.reset_lfo2_freq)
+        self.lfo_phase_display_2.double_clicked.connect(self.reset_lfo2_offset)
+        # menv 1
+        self.menv_att_slider_1.valueChanged.connect(self.change_menv1_att)
+        self.menv_rel_slider_1.valueChanged.connect(self.change_menv1_rel)
+        self.menv_mode_buttons_1.buttonClicked.connect(self.change_menv1_mode)
+        self.menv_att_display_1.double_clicked.connect(self.reset_menv1_att)
+        self.menv_rel_display_1.double_clicked.connect(self.reset_menv1_rel)
+        # menv 2
+        self.menv_att_slider_2.valueChanged.connect(self.change_menv2_att)
+        self.menv_rel_slider_2.valueChanged.connect(self.change_menv2_rel)
+        self.menv_mode_buttons_2.buttonClicked.connect(self.change_menv2_mode)
+        self.menv_att_display_2.double_clicked.connect(self.reset_menv2_att)
+        self.menv_rel_display_2.double_clicked.connect(self.reset_menv2_rel)
+        
 
     #module select slot
     def setModule(self, button):
@@ -322,6 +381,129 @@ class ModulatorGUI(QGroupBox):
             self.mod_stack.setCurrentIndex(2)
         elif button_text == "env 2":
             self.mod_stack.setCurrentIndex(3)
+
+    #module slots
+    # lfo 1
+    def change_lfo1_freq(self, value):
+        norm_value = float(value)/1000.0
+        new_freq = norm_value*10.0
+        self.lfo_freq_display_1.display(f"{new_freq:.2f}")
+        self.lfo1_freq_changed.emit(new_freq)
+
+    def change_lfo1_offset(self, value):
+        norm_value = float(value)/1000.0
+        self.lfo_phase_display_1.display(f"{norm_value:.2f}")
+        self.lfo1_offset_changed.emit(norm_value)
+
+    def change_lfo1_shape(self, button):
+        shape_text = button.text()
+        if shape_text == "sine":
+            new_shape = 0
+        elif shape_text == "tri":
+            new_shape = 1
+        elif shape_text == "ramp":
+            new_shape = 2
+        elif shape_text == "saw":
+            new_shape = 3
+        elif shape_text == "square":
+            new_shape = 4
+        elif shape_text == "s+h":
+            new_shape = 5
+        self.lfo1_shape_changed.emit(new_shape)
+    
+    def reset_lfo1_freq(self):
+        self.lfo_freq_slider_1.setValue(500)
+
+    def reset_lfo1_offset(self):
+        self.lfo_phase_slider_1.setValue(0)
+
+    # lfo 2
+    def change_lfo2_freq(self, value):
+        norm_value = float(value)/1000.0
+        new_freq = norm_value*10.0
+        self.lfo_freq_display_2.display(f"{new_freq:.2f}")
+        self.lfo2_freq_changed.emit(new_freq)
+
+    def change_lfo2_offset(self, value):
+        norm_value = float(value)/1000.0
+        self.lfo_phase_display_2.display(f"{norm_value:.2f}")
+        self.lfo2_offset_changed.emit(norm_value)
+
+    def change_lfo2_shape(self, button):
+        shape_text = button.text()
+        if shape_text == "sine":
+            new_shape = 0
+        elif shape_text == "tri":
+            new_shape = 1
+        elif shape_text == "ramp":
+            new_shape = 2
+        elif shape_text == "saw":
+            new_shape = 3
+        elif shape_text == "square":
+            new_shape = 4
+        elif shape_text == "s+h":
+            new_shape = 5
+        self.lfo2_shape_changed.emit(new_shape)
+    
+    def reset_lfo2_freq(self):
+        self.lfo_freq_slider_2.setValue(500)
+
+    def reset_lfo2_offset(self):
+        self.lfo_phase_slider_2.setValue(0)
+
+    # menv 1
+    def change_menv1_att(self, value):
+        norm_value = float(value)/1000.0
+        self.menv_att_display_1.display(f"{norm_value:.2f}")
+        self.menv1_att_changed.emit(norm_value)
+
+    def change_menv1_rel(self, value):
+        norm_value = float(value)/1000.0
+        self.menv_rel_display_1.display(f"{norm_value:.2f}")
+        self.menv1_rel_changed.emit(norm_value)
+
+    def change_menv1_mode(self, button):
+        mode_text = button.text()
+        if mode_text == "AR":
+            new_mode = 0
+        elif mode_text == "AHR":
+            new_mode = 1
+        elif mode_text == "AR loop":
+            new_mode = 2
+        self.menv1_mode_changed.emit(new_mode)
+
+    def reset_menv1_att(self):
+        self.menv_att_slider_1.setValue(500)
+
+    def reset_menv1_rel(self):
+        self.menv_rel_slider_1.setValue(500)
+    
+    # menv 2
+    def change_menv2_att(self, value):
+        norm_value = float(value)/1000.0
+        self.menv_att_display_2.display(f"{norm_value:.2f}")
+        self.menv2_att_changed.emit(norm_value)
+
+    def change_menv2_rel(self, value):
+        norm_value = float(value)/1000.0
+        self.menv_rel_display_2.display(f"{norm_value:.2f}")
+        self.menv2_rel_changed.emit(norm_value)
+
+    def change_menv2_mode(self, button):
+        mode_text = button.text()
+        if mode_text == "AR":
+            new_mode = 0
+        elif mode_text == "AHR":
+            new_mode = 1
+        elif mode_text == "AR loop":
+            new_mode = 2
+        self.menv2_mode_changed.emit(new_mode)
+
+    def reset_menv2_att(self):
+        self.menv_att_slider_2.setValue(500)
+
+    def reset_menv2_rel(self):
+        self.menv_rel_slider_2.setValue(500)
 
     #helpers
     def configure_display(self, display, num_digits, num_mode, dig_style, small_dec):
@@ -419,7 +601,8 @@ class CoolDial(QDial):
         event.accept()
 
     def mouseDoubleClickEvent(self, event):
-        self.setValue(0)
+        if (event.button() == Qt.MouseButton.LeftButton):
+            self.setValue(0)
         event.accept()
 
         
