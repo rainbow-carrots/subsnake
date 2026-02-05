f06c96bb9d534f3e0f12615dee3deb9ded06c442
rae <ramonaauble@gmail.com>
2026-02-04 21:59:11 -0500

    add constants (*oneoverfs*, *piovertwo*, *max_det_inc*) **WrappedOsc**:  *process_block()*: update *generate_sine()* and *generate_saw()* to pass *fs* arg, replace block-based pulse with sample-based implementation (*polyblep_pulse()*)   *generate_sine()* & *polyblep_saw()*: add *freq* arg, calc *base_inc* at start of block, implement pitch modulation  *polyblep_pulse()*: add numba function for modulated polyblep pulse

--- subsnake/audio/generators.py
+++ subsnake/audio/generators.py
@@ -3,8 +3,11 @@ import math
 from numba import njit
 
 fs = 44100
+oneoverfs = 1.0/float(fs)
 twopi = 2*np.pi
 oneoverpi = 1/np.pi
+piovertwo = np.pi/2.0
+max_det_inc = twopi*(10.0/float(fs))
 
 # phase-wrapped oscillator
 # alg = 0.0: sine, 1.0: polyblep ramp, 2.0: polyblep pulse | width = 0.0 to 1.0
@@ -22,18 +25,11 @@ class WrappedOsc():
     def process_block(self, buffer, mod_buffers, mod_values):
         #print(f"DEBUG: buffer shape is {buffer.shape}")
         if (self.alg == 0):
-            self.generate_sine(self.state, buffer, mod_buffers[0], mod_buffers[1], mod_buffers[2], mod_values[0], mod_values[1], mod_values[2], self.amp)
+            self.generate_sine(self.state, buffer, mod_buffers[0], mod_buffers[1], mod_buffers[2], mod_values[0], mod_values[1], mod_values[2], self.amp, self.freq)
         elif (self.alg == 1.0):
-            self.polyblep_saw(self.state, buffer, mod_buffers[0], mod_buffers[1], mod_buffers[2], mod_values[0], mod_values[1], mod_values[2], self.amp)
+            self.polyblep_saw(self.state, buffer, mod_buffers[0], mod_buffers[1], mod_buffers[2], mod_values[0], mod_values[1], mod_values[2], self.amp, self.freq)
         elif (self.alg == 2.0):
-            #anti-aliased square wave
-            frames = len(buffer)
-            self.state2[0] = self.state[0] + self.pulsewidth*twopi
-            if (self.state2[0] > twopi):
-                self.state2[0] -= twopi
-            self.polyblep_saw(self.state, buffer, mod_buffers[0], mod_buffers[1], mod_buffers[2], mod_values[0], mod_values[1], mod_values[2], self.amp)
-            self.polyblep_saw(self.state2, self.hardSyncBuffer, mod_buffers[0], mod_buffers[1], mod_buffers[2], mod_values[0], mod_values[1], mod_values[2], self.amp)
-            buffer[:] -= self.hardSyncBuffer[:frames]
+            self.polyblep_pulse(self.state, buffer, self.state2, self.pulsewidth, mod_buffers[0], mod_buffers[1], mod_buffers[2], mod_buffers[3], mod_values[0], mod_values[1], mod_values[2], mod_values[3], self.amp, self.freq)
 
     def update_pitch(self, newPitch):
         self.freq = newPitch
@@ -56,8 +52,10 @@ class WrappedOsc():
     #phase-wrapped sine wave
     @staticmethod
     @njit(nogil=True, fastmath=True)
-    def generate_sine(state, outdata, pitch_mod, det_mod, amp_mod, pm_amt, dm_amt, am_amt, amp=1.0):
+    def generate_sine(state, outdata, pitch_mod, det_mod, amp_mod, pm_amt, dm_amt, am_amt, amp=1.0, freq=440.0):
+        base_inc = twopi*freq*oneoverfs
         for n in range(len(outdata)):
+            state[2] = base_inc + base_inc*pitch_mod[n]*pm_amt + max_det_inc*det_mod[n]*dm_amt
             sample = state[1] * math.sin(state[0])
             outdata[n][0] = sample*(amp - amp_mod[n]*am_amt)
             outdata[n][1] = sample*(amp - amp_mod[n]*am_amt)
@@ -68,9 +66,13 @@ class WrappedOsc():
     #anti-aliased sawtooth
     @staticmethod
     @njit(nogil=True, fastmath=True)
-    def polyblep_saw(state, outdata, pitch_mod, det_mod, amp_mod, pm_amt, dm_amt, am_amt, amp=1.0):
+    def polyblep_saw(state, outdata, pitch_mod, det_mod, amp_mod, pm_amt, dm_amt, am_amt, amp=1.0, freq=440.0):
         frames = len(outdata)
+        base_inc = twopi*freq*oneoverfs
         for n in range(frames):
+            #modulate phase increment
+            state[2] = base_inc + base_inc*pitch_mod[n]*pm_amt + max_det_inc*det_mod[n]*dm_amt
+
             #generate naive saw
             sample = (state[0] * oneoverpi) - 1.0
             
@@ -88,6 +90,51 @@ class WrappedOsc():
                 state[0] -= twopi
 
             #output
+            sample *= state[1]
+            outdata[n, 0] = sample*(amp - amp_mod[n]*am_amt)
+            outdata[n, 1] = sample*(amp - amp_mod[n]*am_amt)
+
+    #anti-aliased pulse
+    @staticmethod
+    @njit(nogil=True, fastmath=True)
+    def polyblep_pulse(state, outdata, state2, width, pitch_mod, det_mod, amp_mod, width_mod, pm_amt, dm_amt, am_amt, wm_amt, amp=1.0, freq=440.0):
+        frames = len(outdata)
+        base_inc = twopi*freq*oneoverfs
+        for n in range(frames):
+            #modulate phase increment
+            state[2] = base_inc + base_inc*pitch_mod[n]*pm_amt + max_det_inc*det_mod[n]*dm_amt
+
+            #generate naive saws
+            sample = (state[0] * oneoverpi) - 1.0
+            sample2 = (state2[0]* oneoverpi) - 1.0
+            
+            #apply polyblep corrections
+            # main osc
+            if (state[0] < state[2]):           #phase just wrapped
+                t = state[0]/state[2]
+                sample += (t - 1.0)**2
+            elif (state[0] + state[2] > twopi):   #phase about to wrap
+                t = (twopi - state[0])/state[2]
+                sample -= (t - 1.0)**2
+            
+            # sync osc
+            if (state2[0] < state2[2]):           #phase just wrapped
+                t2 = state2[0]/state2[2]
+                sample2 += (t2 - 1.0)**2
+            elif (state2[0] + state2[2] > twopi):   #phase about to wrap
+                t2 = (twopi - state2[0])/state2[2]
+                sample2 -= (t2 - 1.0)**2
+
+            #increment phases & wrap
+            state[0] += state[2]
+            state2[0] = state[0] + twopi*width + piovertwo*width_mod[n]*wm_amt
+            if (state[0] > twopi):
+                state[0] -= twopi
+            if (state2[0] > twopi):
+                state2[0] -= twopi
+
+            #output
+            sample -= sample2
             sample *= state[1]
             outdata[n, 0] = sample*(amp - amp_mod[n]*am_amt)
             outdata[n, 1] = sample*(amp - amp_mod[n]*am_amt)
