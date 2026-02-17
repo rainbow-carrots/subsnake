ef8b4649e774f9b26a4e643ad633ea09fb11a972
rae <ramonaauble@gmail.com>
2026-02-17 16:03:24 -0500

    **WrappedOsc**:   *process_block()*: change *polyblep_pulse()* -> *blit_pulse()*   *blit_pulse()*: add BLIT-based pulse waveform generator

--- subsnake/audio/generators.py
+++ subsnake/audio/generators.py
@@ -42,7 +42,8 @@ class WrappedOsc():
             self.blit_saw(buffer, self.blit_buffer, self.blit_buffer_write, self.blit_states, self.blit_integrators, self.hermite_interpolate,  
                           self.random_walk[:frames], self.walk_amt, mod_buffers[0], mod_buffers[1], mod_buffers[2], mod_values[0], mod_values[1], mod_values[2], self.amp, self.freq)
         elif (self.alg == 2.0):
-            self.polyblep_pulse(self.state, buffer, self.state2, self.pulsewidth, self.random_walk[:frames], self.walk_amt, mod_buffers[0], mod_buffers[1], mod_buffers[2], mod_buffers[3], mod_values[0], mod_values[1], mod_values[2], mod_values[3], self.amp, self.freq)
+            self.blit_pulse(buffer, self.blit_buffer, self.blit_buffer_write, self.blit_states, self.blit_integrators, self.hermite_interpolate,  
+                          self.random_walk[:frames], self.walk_amt, mod_buffers[0], mod_buffers[1], mod_buffers[2], mod_buffers[3], mod_values[0], mod_values[1], mod_values[2], mod_values[3], self.amp, self.freq, self.pulsewidth)
 
     def update_pitch(self, newPitch):
         self.freq = newPitch
@@ -186,6 +187,61 @@ class WrappedOsc():
                 else:
                     slope = 1.0-math.sin(np.pi*harmonics*phase)/kernel_den
                 slope *= increment*2
+                integrators[0, c] = integrators[0, c]*leak_c + slope
+                states[c, 0] += increment
+                states[c, 0] -= np.floor(states[c, 0])
+
+                buffer[write_heads[0, c], c] = integrators[0, c]
+                base_delay = period + 2.0
+                total_delay = base_delay + pm_amt*pitch_mod[n]*period
+                int_offset = np.floor(total_delay)
+                frac_offset = total_delay - int_offset
+                read_head = write_heads[0, c] - int(int_offset)
+                read_head = read_head % buffer_len
+                y0_index = read_head-1
+                if y0_index < 0:
+                    y0_index += buffer_len
+                y2_index = read_head+1
+                if y2_index >= buffer_len:
+                    y2_index -= buffer_len
+                y3_index = read_head+2
+                if y3_index >= buffer_len:
+                    y3_index -= buffer_len
+                mod_amp = max(-1.0, min(1.0, amp + amp_mod[n]*am_amt))
+                outdata[n, c] = hermite_interpolate(buffer[y0_index, c], buffer[read_head, c], buffer[y2_index, c], buffer[y3_index, c], frac_offset)*mod_amp
+                write_heads[0, c] += 1
+                if write_heads[0, c] >= buffer_len:
+                    write_heads[0, c] -= buffer_len
+
+    @staticmethod
+    @njit(nogil=True, fastmath=True)
+    def blit_pulse(outdata, buffer, write_heads, states, integrators, hermite_interpolate,
+                    walk_mod, walk_amt, pitch_mod, det_mod, amp_mod, width_mod, pm_amt, dm_amt, am_amt, wm_amt, amp=1.0, freq=440.0, width=0.5):
+        frames = len(outdata)
+        buffer_len = len(buffer)
+        for n in range(0, frames):
+            for c in range(0, 2):
+                increment = freq*oneoverfs + max_det_inc*walk_mod[n]*walk_amt + max_det_inc*det_mod[n]*dm_amt
+                increment = max(1e-9, increment)
+                period = 1/increment
+                new_freq = increment*fs
+                harmonics = 2*int(nyquist/new_freq) + 1
+                phase1 = states[c, 0]
+                new_width = max(0.0, min(1.0, width + width_mod[n]*wm_amt))
+                phase2 = phase1 + new_width
+                phase2 -= np.floor(phase2)
+                leak_c = 1.0 - twopi*increment*.1
+                kernel_den_rise = math.sin(np.pi*phase1)
+                kernel_den_fall = math.sin(np.pi*phase2)
+                if phase1 < .0000001:
+                    slope_rise = harmonics
+                else:
+                    slope_rise = math.sin(np.pi*harmonics*phase1)/kernel_den_rise
+                if phase2 < .0000001:
+                    slope_fall = harmonics
+                else:
+                    slope_fall = math.sin(np.pi*harmonics*phase2)/kernel_den_fall
+                slope = (slope_rise - slope_fall)*increment*2
                 integrators[0, c] = integrators[0, c]*leak_c + slope
                 states[c, 0] += increment
                 states[c, 0] -= np.floor(states[c, 0])
