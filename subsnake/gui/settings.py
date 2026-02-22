bfd9a881586600aa16c9010a02dc747fade041ce
rae <ramonaauble@gmail.com>
2026-02-22 18:53:13 -0500

    import **QPushButton**, **QDialog**, **QDialogButtonBox**, **QLineEdit** **RecorderSettings**: add recorder settings class   *init()*: add widgets, init user dir, connect internal signals   *save_file()*: helper to get filename from dialog & emit with *save_rec_buffer* signal   *check_user_dir()*: helper to create user sound file directory **SaveRecDialog**: add dialog class to get recording filename   *init()*: add line edit & ok/cancel buttons, connect internal signals, set title/object name   *get_name()*: helper to get name from dialog line edit widget   *clear_name()*: helper to clear dialog name entry box

--- subsnake/gui/settings.py
+++ subsnake/gui/settings.py
@@ -1,10 +1,13 @@
 from PySide6.QtWidgets import (
-    QGroupBox, QGridLayout,
-    QLabel, QSlider, QLCDNumber
+    QGroupBox, QGridLayout, QPushButton,
+    QLabel, QSlider, QLCDNumber,
+    QDialog, QDialogButtonBox, QLineEdit
 )
 from PySide6.QtCore import Signal, Qt
 from PySide6.QtGui import QColor, QPalette
 from subsnake.gui.lcd import ClickLCD
+from platformdirs import user_data_dir
+from pathlib import Path
 
 class SynthSettings(QGroupBox):
     drift_changed = Signal(float)
@@ -59,6 +62,60 @@ class SynthSettings(QGroupBox):
         display.setAutoFillBackground(True)
         display.setPalette(display_palette)
 
+class RecorderSettings(QGroupBox):
+    save_rec_buffer = Signal(str)
 
+    def __init__(self, display_color=QColor("black")):
+        super().__init__()
+        self.save_button = QPushButton("save")
+        self.save_rec_dialog = SaveRecDialog()
 
+        layout = QGridLayout()
+        layout.addWidget(self.save_button)
 
+        self.setLayout(layout)
+        self.setObjectName("record_group")
+        self.setTitle("record settings")
+
+        self.user_sound_path = Path(user_data_dir("subsnake", "rainbow-carrots")) / "sound"
+        self.check_user_dir()
+
+        self.save_button.clicked.connect(self.save_file)
+
+    def save_file(self):
+        if self.save_rec_dialog.exec():
+            new_name = self.save_rec_dialog.get_name()
+            if (new_name != ""):
+                new_filename = new_name + ".wav"
+                new_path = self.user_sound_path / new_filename
+                new_str_path = str(new_path)
+                self.save_rec_buffer.emit(new_str_path)
+        self.save_rec_dialog.clear_name()
+    
+    def check_user_dir(self):
+        self.user_sound_path.mkdir(parents=True, exist_ok=True)
+
+class SaveRecDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.name_input = QLineEdit()
+        self.buttons = QDialogButtonBox(QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
+        self.buttons.setFocusPolicy(Qt.NoFocus)
+
+        layout = QGridLayout()
+        layout.addWidget(self.name_input, 0, 0)
+        layout.addWidget(self.buttons, 1, 0)
+
+        self.buttons.accepted.connect(self.accept)
+        self.buttons.rejected.connect(self.reject)
+
+        self.setLayout(layout)
+        self.setObjectName("save_rec_dialog")
+        self.setWindowTitle("enter filename:")
+
+    def get_name(self):
+        return self.name_input.text()
+    
+    def clear_name(self):
+        self.name_input.setText("")
