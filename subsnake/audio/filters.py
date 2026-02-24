82a77a804dfe449004d6822d9682036a6d64fcc9
rae <ramonaauble@gmail.com>
2026-02-23 22:01:06 -0500

    **HalSVF()*:   *init()*: add *base_freq* & *key_tracking* attributes, update *f* coefficient init line   *process_block()*: pass *base_freq* & *key_tracking* to njit *filter_block()* call   *update_key_tracking()*, *update_base_freq()*: add setters for key tracking amount & base frequency   *filter_block()*: add *base_freq* & *kt_amt* args, calc. cutoff & keytrack frequency contribution (before loop), update *new_cutoff* (in loop) with *cutoff_freq_amt* and *kt_freq_amt*
      enable filter cutoff frequency key tracking

--- subsnake/audio/filters.py
+++ subsnake/audio/filters.py
@@ -6,7 +6,7 @@ fs = 44100
 twopi = 2*np.pi
 oneoverpi = 1/np.pi
 
-# Hal Chamberlin's digital SV Filter w/ nonlinear feedback | 4x oversampled
+# Hal Chamberlin's digital SV Filter w/ nonlinear feedback | 8x oversampled
 # type = 1.0: lowpass, 2.0: highpass, 3.0: bandpass, 4.0: notch
 class HalSVF():
     def __init__(self, type, cutoff, resonance, drive=1.0, saturate=8.0):
@@ -15,15 +15,17 @@ class HalSVF():
         self.drive = drive
         self.saturate = saturate
         self.env_amount = 0.0
+        self.base_freq = 440.0
+        self.key_tracking = 0.0
 
         q = resonance
-        f = 2*math.sin(np.pi*(cutoff/(4*fs)))
+        f = 2*math.sin(np.pi*(cutoff/(8*fs)))
 
         #init parameter buffer | lowpass, bandpass, tuning, dampening, type, drive, saturation
         self.state = np.array([[0.0, 0.0, f, q, type, drive, saturate], [0.0, 0.0, f, q, type, drive, saturate]], dtype=np.float32)
     
     def process_block(self, input, output, fenv, mod_buffer, mod_values):
-        self.filter_block(self.state, input, output, fenv, self.env_amount, HalSVF.clip_sample, self.cutoff,
+        self.filter_block(self.state, input, output, fenv, self.env_amount, HalSVF.clip_sample, self.cutoff, self.base_freq, self.key_tracking,
                           mod_buffer[0], mod_buffer[1], mod_buffer[2], mod_buffer[3], mod_buffer[4], mod_values[0], mod_values[1], mod_values[2], mod_values[3], mod_values[4])
 
     def update_cutoff(self, freq):
@@ -47,10 +49,18 @@ class HalSVF():
 
     def update_env_amount(self, amount):
         self.env_amount = amount
+
+    def update_key_tracking(self, amount):
+        self.key_tracking = amount
+
+    def update_base_freq(self, newFreq):
+        self.base_freq = newFreq
         
     @staticmethod
     @njit(nogil=True, fastmath=True)
-    def filter_block(state, input, output, fenv, fenv_amount, clip_sample, cutoff, freq_mod, res_mod, drive_mod, sat_mod, fenv_mod, fm_val, rm_val, dm_val, sm_val, fem_val):
+    def filter_block(state, input, output, fenv, fenv_amount, clip_sample, cutoff, base_freq, kt_amt, freq_mod, res_mod, drive_mod, sat_mod, fenv_mod, fm_val, rm_val, dm_val, sm_val, fem_val):
+        cutoff_freq_amt = (1.0 - kt_amt)*cutoff
+        kt_freq_amt = kt_amt*base_freq*32.0
         for c in range (2):
             for n in range(len(output)):
                 freq_mod_amt = freq_mod[n]*fm_val
@@ -61,7 +71,7 @@ class HalSVF():
                 prev_low = state[c, 0]
                 prev_band = state[c, 1]
                 fenv_amount = max(-1.0, min(1.0, fenv_amount + fenv_mod[n]*fem_val))
-                new_cutoff = max(0.1, min(cutoff + 14080.0*freq_mod_amt + 14080.0*fenv[n, c]*fenv_amount, 14080.0))
+                new_cutoff = max(0.1, min(cutoff_freq_amt + kt_freq_amt + 14080.0*freq_mod_amt + 14080.0*fenv[n, c]*fenv_amount, 14080.0))
                 freq_c = 2*math.sin(np.pi*(new_cutoff/(8*fs)))
                 res_c = max(.02, min(20.0, state[c, 3] + res_mod_amt))
                 substate = int(state[c, 4])
