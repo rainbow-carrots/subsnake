067c1df605b9a1a1e981658d1670b5fd8cdcb126
rae <ramonaauble@gmail.com>
2026-02-23 22:09:57 -0500

    update **HalSVF**.*filter_block()* init njit compile call (pass test vals for new *base_freq* & *kt_amt* args), change init cutoff from int -> float

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ WrappedOsc.blit_pulse(test_blit_out, test_blit_states, test_blit_integrators, te
                     walk_test, 1.0, mod_test, mod_test, mod_test, mod_test, 0, 0, 0, 0, 0.5, 440.0, 0.5)
 WrappedOsc.blit_triangle(test_blit_out, test_blit_states, test_blit_integrators, test_smoothed_widths,
                     walk_test, 1.0, mod_test, mod_test, mod_test, mod_test, 0, 0, 0, 0, 0.5, 440.0, 0.5)
-HalSVF.filter_block(filt_test, np.zeros((16, 2), dtype=np.float32), np.zeros((16, 2), dtype=np.float32), np.ones((16, 2), dtype=np.float32), 0.0, HalSVF.clip_sample, 100,
+HalSVF.filter_block(filt_test, np.zeros((16, 2), dtype=np.float32), np.zeros((16, 2), dtype=np.float32), np.ones((16, 2), dtype=np.float32), 0.0, HalSVF.clip_sample, 100.0, 100.0, 0.0,
                     mod_test, mod_test, mod_test, mod_test, mod_test, 0, 0, 0, 0, 0)
 ADSR.envelope_block(env_test, False, np.zeros((16, 2), dtype=np.float32), np.zeros((16, 2), dtype=np.float32), 0, 0, mod_test, mod_test, mod_test, mod_test, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
 AudioRecorder.process_samples(np.zeros((32, 2), dtype=np.float32), np.zeros((16, 2), dtype=np.float32), np.zeros((16, 2), dtype=np.float32), 0,
