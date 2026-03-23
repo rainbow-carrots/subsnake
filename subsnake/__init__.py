5c1f4fc02e8bcd46ffd553135485e07b0d2b9447
rae <ramonaauble@gmail.com>
2026-03-23 18:12:10 -0400

    expose **ZDFSVF**, **LFO** and **ModEnv** classes to main module

--- subsnake/__init__.py
+++ subsnake/__init__.py
@@ -1,5 +1,6 @@
 from .gui.window import MainWindow
 from .audio.engine import AudioEngine
 from .audio.generators import WrappedOsc
-from .audio.filters import HalSVF
+from .audio.filters import HalSVF, ZDFSVF
 from .audio.envelopes import ADSR
+from .audio.modulators import LFO, ModEnv
