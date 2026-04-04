e1ec396ce528c6f68c24a8284f8672a0aff9bd65
rae <ramonaauble@gmail.com>
2026-04-04 13:34:20 -0400

    expose **Panner** class

--- subsnake/audio/__init__.py
+++ subsnake/audio/__init__.py
@@ -3,6 +3,6 @@ from .generators import WrappedOsc
 from .envelopes import ADSR
 from .engine import AudioEngine
 from .workers import KeyEventWorker
-from .effects import StereoDelay, AudioRecorder
+from .effects import StereoDelay, AudioRecorder, Panner
 from .modulators import LFO, ModEnv
 from .voice import Voice
